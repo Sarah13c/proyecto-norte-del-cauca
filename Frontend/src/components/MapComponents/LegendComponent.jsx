f28d8909b3ec30ad69065c26b64a3dd948635850
JeanLGH <103538027+JeanLGH@users.noreply.github.com>
2024-05-18 14:49:49 -0500

    s

--- Frontend/src/components/MapComponents/LegendComponent.jsx
+++ Frontend/src/components/MapComponents/LegendComponent.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import '../../assets/css/LegendStyles.css'; 
+import '../../assets/css/LegendStyles.css'; // Asegúrate de tener estilos CSS para el componente Legend
 
-const LegendComponent = () => {
-    const municipios = [
-      { name: 'Santander de Quilichao', color: '#7c1d6f', location: [3.0136, -76.4833] },
-      { name: 'Guachené', color: '#b9257a', location: [3.0369, -76.4644] },
-      { name: 'Puerto Tejada', color: '#dc3977', location: [3.2314, -76.4225] }
-    ];
-  
-    return (
-      <div className="legend-container">
-        <h4>Leyenda de Municipios</h4>
-        <ul>
-          {municipios.map((municipio, index) => (
-            <li key={index}>
-              <span className="legend-color" style={{ backgroundColor: municipio.color }} />
-              {municipio.name}
-            </li>
-          ))}
-        </ul>
+const LegendComponent = ({ municipality, population }) => {
+  return (
+    <div className="legend-container">
+      <div className="legend-item">
+        <div className="legend-text">{municipality}</div>
+        <div className="legend-text">Población: {population}</div>
       </div>
-    );
-  };
-  
-  export default LegendComponent;
+    </div>
+  );
+};
+
+export default LegendComponent;
--- Frontend/src/components/MapComponents/MapComponent.jsx
+++ Frontend/src/components/MapComponents/MapComponent.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import PolygonComponent from './PolygonComponent.jsx';
 import { statesData } from '../../files/data';
-import 'leaflet/dist/leaflet.css';
+import Legend from './LegendComponent.jsx'; // Importamos el componente Legend
 import '../../styles/MapStyles.css';
-
 
 export default function MapComponent({ center, mousePosition, setMousePosition }) {
   return (
     <div className="map-container">
+      {/* Agregamos el componente Legend fuera del MapContainer */}
+      <Legend />
       <div style={{ width: '800px', height: '700px' }}>
         <MapContainer
           center={center}
@@ -18,9 +19,9 @@ export default function MapComponent({ center, mousePosition, setMousePosition }
         >
           <TileLayer
             url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=bPH7ayo05grPl639LD7n"
-
             attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
           />
+          <GeoJSON data={statesData} />
           {statesData.features.map((state) => (
             <PolygonComponent
               key={state.id}
