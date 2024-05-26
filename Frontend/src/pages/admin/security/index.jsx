a4601ea0dd83ca5a17130fc2f1614f3a6e1aa2d2
Sarah Cano <sarahcb13@hotmail.com>
2024-05-26 12:15:49 -0500

    Update Security Index

--- Frontend/src/pages/admin/security/index.jsx
+++ Frontend/src/pages/admin/security/index.jsx
@@ -17,6 +17,11 @@ import MapComponent from "../../../components/MapComponents/MapComponent";
 import TotalAccesosCarnales from "../../admin/security/components/TotalAccesosCarnales";
 //homicidios
 import HomicidiosPorMunicipio from "../../admin/security/components/HomicidiosPorMunicipio";
+//Lesiones
+import TotalLesiones from "./components/TotalLesiones";
+//Hurtos
+import TotalHurtos from "./components/TotalHurtos";
+
 
 export default function SecurityReports() {
 
@@ -26,6 +31,13 @@ export default function SecurityReports() {
   //acceos carnales
   const [dataAccesos, setDataAccesos] = useState([]);
   const [areas, setAreas] = useState([]);
+
+  // Hurtos
+  const [dataHurtos, setDataHurtos] = useState([]);
+
+  // Hurtos
+  const [dataLesiones, setDataLesiones] = useState([]);
+
 
   //homicidios
   const [dataHomicidios, setDataHomicidios] = useState([]);
@@ -68,7 +80,42 @@ export default function SecurityReports() {
     fetchDataHomicidios();
   }, []);
 
+  // Fetch data Lesiones
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/lesionesPersonales');
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos del servidor");
+        }
+        const data = await response.json();
+        setDataLesiones(data);
+      } catch (error) {
+        console.error("Error al obtener los datos del servidor:", error);
+      }
+    };
 
+    fetchData();
+  }, []);
+
+
+   // Fetch data Hurtos
+   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/hurtos1922');
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos del servidor");
+        }
+        const data = await response.json();
+        setDataHurtos(data);
+      } catch (error) {
+        console.error("Error al obtener los datos del servidor:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   
   
   
@@ -78,7 +125,7 @@ export default function SecurityReports() {
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 2, "2xl": 2 }}
+        columns={{ base: 1, md: 3, lg: 3, "2xl": 3 }}
         gap='20px'
         mb='20px'
       >
@@ -93,8 +140,22 @@ export default function SecurityReports() {
               }
             />
           }
-          name={`Nacimientos de Hombres en `}
-          value={22}
+          name={`oli`}
+          value={2020}
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w='56px'
+              h='56px'
+              bg={boxBg}
+              icon={
+                <Icon w='32px' h='32px' as={BsGenderMale} color={brandColor} />
+              }
+            />
+          }
+          name={`Nacimientos de Hombres en 2022`}
+          value={20202}
         />
         <MiniStatistics
           startContent={
@@ -107,8 +168,8 @@ export default function SecurityReports() {
               }
             />
           }
-          name={`Nacimientos de Mujeres en 22`}
-          value={22}
+          name={`Nacimientos de Mujeres en 2021`}
+          value={4334}
         />
       </SimpleGrid>
       <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
@@ -117,15 +178,15 @@ export default function SecurityReports() {
           areas={areas}
           onAreaChange={() => { }}
         />
+
+      <TotalLesiones data={dataLesiones} /> 
        
       </SimpleGrid>
       <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap="20px" mb="20px">
         <AspectRatio ratio={16 / 9}>
-          <MapComponent
-            center={center}
-            mousePosition={mousePosition}
-            setMousePosition={setMousePosition}
-          />
+        <TotalHurtos
+          data={dataHurtos}          
+        />
         </AspectRatio>
         <SimpleGrid columns={{ base: 1, md: 2, xl: 1 }} gap="20px">
           <HomicidiosPorMunicipio data={dataHomicidios} />
