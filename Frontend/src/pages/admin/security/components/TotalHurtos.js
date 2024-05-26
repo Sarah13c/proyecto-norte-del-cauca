39bf562195601ed3ba6532112dba4b8a210e6e61
Sarah Cano <sarahcb13@hotmail.com>
2024-05-26 12:15:22 -0500

    Add Total Hurtos

---
+++ Frontend/src/pages/admin/security/components/TotalHurtos.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Box, Flex, Select, Text, useColorModeValue } from '@chakra-ui/react';
+import Card from '../../../../components/card/Card.js';
+import LineApexChart from '../../../../components/charts/LineApexChart.js';
+
+export default function TotalHurtos({ data, ...rest }) {
+  const textColor = useColorModeValue('secondaryGray.900', 'white');
+  const [selectedZone, setSelectedZone] = useState('RURAL'); // Estado para almacenar la zona seleccionada
+
+  // Función para filtrar los datos según la zona seleccionada
+  const filterDataByZone = (data, zone) => {
+    return data.filter((item) => item.ZONA_Hurto === zone);
+  };
+
+  // Datos filtrados según la zona seleccionada
+  const filteredData = selectedZone ? filterDataByZone(data, selectedZone) : data;
+
+  return (
+    <Card align="center" direction="column" w="100%" {...rest}>
+      <Flex align="center" w="100%" px="15px" py="10px">
+        <Text me="auto" color={textColor} fontSize="xl" fontWeight="700" lineHeight="100%">
+          Hurtos por Municipio y Zona
+        </Text>
+        <Select
+          value={selectedZone}
+          onChange={(e) => setSelectedZone(e.target.value)}
+          placeholder="Selecciona una zona"
+        >
+          <option value="RURAL">Rural</option>
+          <option value="URBANA">Urbana</option>
+        </Select>
+      </Flex>
+      <Box h="350px" mt="auto">
+        {filteredData && <LineApexChart data={filteredData} />}
+      </Box>
+    </Card>
+  );
+}
