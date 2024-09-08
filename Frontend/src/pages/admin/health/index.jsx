7fb390e8dc847ecad1cd8db41af994ec7b6d1848
Sarah Cano <sarahcb13@hotmail.com>
2024-09-08 13:06:16 -0500

    Fix Card Map

--- Frontend/src/pages/admin/health/index.jsx
+++ Frontend/src/pages/admin/health/index.jsx
@@ -4,9 +4,11 @@ import {
   Icon,
   SimpleGrid,
   useColorModeValue,
-  AspectRatio,
-  Select,
+  Text,
+  Flex,
+
 } from "@chakra-ui/react";
+import Card from "../../../components/card/Card";
 import '../../../assets/css/App.css';
 import MiniStatistics from "../../../components/card/MiniStatistics";
 import IconBox from "../../../components/icons/IconBox";
@@ -166,13 +168,21 @@ export default function HealthReports() {
         />
       </SimpleGrid>
       <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap="20px" mb="20px">
-        <AspectRatio ratio={16 / 9}>
-          <MapComponent
-            center={center}
-            mousePosition={mousePosition}
-            setMousePosition={setMousePosition}
-          />
-        </AspectRatio>
+      <Card justifyContent="center" align="center" direction="column" w="100%" mb="0px">
+          <Flex align="center" w="100%">
+            <Text me="auto" fontSize="xl" fontWeight="700" lineHeight="100%">
+              Mapa Demográfico del Norte del Cauca
+            </Text>
+          </Flex>
+          .
+          <Box minH="260px" minW="100%" mt="auto">
+            <MapComponent
+              center={center} 
+              mousePosition={mousePosition}
+              setMousePosition={setMousePosition}
+            />
+          </Box>
+        </Card>
         <SimpleGrid columns={{ base: 1, md: 2, xl: 1 }} gap="20px">
           <TotalNacimientos
             nacimientosData={nacimientosData}
